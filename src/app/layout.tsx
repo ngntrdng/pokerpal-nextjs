3553923a9b4d7500df2647d39e66ae3ad3809f29
chris ng <ngntrdng85@gmail.com>
2025-11-30 18:12:42 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,8 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <script src="https://telegram.org/js/telegram-web-app.js"></script>
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <script src="https://telegram.org/js/telegram-web-app.js"></script>
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
